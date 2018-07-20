9db3cef8dc0ce16ad392a16d329e0c6a33c545be
Maximilian Lupke <maxlupke@gmail.com>
2018-07-20 19:51:04 +0200

    Try to fix Travis builds

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,13 +18,13 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin hello --target $TARGET --release -- -C lto
+    cross rustc --target $TARGET --release -- -C lto -C target-feature=$FEATURES
 
     # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/hello $stage/
+    cp target/$TARGET/release/asymptote $stage/
 
     cd $stage
-    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET$FEATURES.tar.gz *
     cd $src
 
     rm -rf $stage
