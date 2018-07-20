848615b7efcc0ee7a368b00b291a758de73f4c19
Maximilian Lupke <maxlupke@gmail.com>
2018-07-20 20:34:12 +0200

    Fix travis (hopefully)

--- ci/script.sh
+++ ci/script.sh
@@ -4,15 +4,15 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET -- -C target-feature=$FEATURES
-    cross build --target $TARGET --release -- -C target-feature=$FEATURES
+    RUSTFLAGS="-C target-feature=$FEATURES" cross build --target $TARGET
+    RUSTFLAGS="-C target-feature=$FEATURES" cross build --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --target $TARGET -- -C target-feature=$FEATURES
-    cross test --target $TARGET --release -- -C target-feature=$FEATURES
+    RUSTFLAGS="-C target-feature=$FEATURES" cross test --target $TARGET
+    RUSTFLAGS="-C target-feature=$FEATURES" cross test --target $TARGET
 
     # cross run --target $TARGET
     # cross run --target $TARGET --release
