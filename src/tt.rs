44056d94ee5c720b85fb44aabd1857a799fee885
Maximilian Lupke <maxlupke@gmail.com>
2019-03-30 07:40:01 +0100

    Reorder contents of tt module
    No functional change.

--- src/tt.rs
+++ src/tt.rs
@@ -23,152 +23,11 @@ use crate::search::*;
 
 use std::cmp;
 
-#[repr(align(64))]
-pub struct Bucket([TTEntry; NUM_CLUSTERS]);
-
 pub struct TT {
     table: Vec<Bucket>,
     bitmask: u64,
     generation: u8,
 }
-
-pub type Bound = u8;
-pub const LOWER_BOUND: Bound = 1;
-pub const UPPER_BOUND: Bound = 2;
-pub const EXACT_BOUND: Bound = LOWER_BOUND | UPPER_BOUND;
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub struct TTScore(Score);
-
-impl TTScore {
-    pub fn to_score(self, ply: Ply) -> Score {
-        if self.0 < -MATE_SCORE + MAX_PLY {
-            self.0 + ply
-        } else if self.0 > MATE_SCORE - MAX_PLY {
-            self.0 - ply
-        } else {
-            self.0
-        }
-    }
-
-    pub fn from_score(score: Score, ply: Ply) -> TTScore {
-        if score < -MATE_SCORE + MAX_PLY {
-            TTScore(score - ply)
-        } else if score > MATE_SCORE - MAX_PLY {
-            TTScore(score + ply)
-        } else {
-            TTScore(score)
-        }
-    }
-}
-
-#[derive(Copy, Clone, PartialEq, Eq, Debug)]
-pub struct TTEntry {
-    key: Hash,             // 8 byte
-    pub best_move: TTMove, // 2 byte
-    pub depth: Depth,      // 2 byte
-    pub score: TTScore,    // 2 byte
-    pub bound: Bound,      // 1 byte
-    generation: u8,        // 1 byte
-}
-
-impl Default for TTEntry {
-    fn default() -> Self {
-        TTEntry {
-            key: 0,
-            depth: 0,
-            score: TTScore(0),
-            best_move: TTMove { from: 0, to: 0 },
-            bound: 0,
-            generation: 0,
-        }
-    }
-}
-
-#[derive(Copy, Clone, PartialEq, Eq, Debug)]
-pub struct TTMove {
-    from: u8, // bits 0-5: from positions, bit 6: capture, bit 7: en passant
-    to: u8,   // bits 0-5: to position, bits 6-7 promotion piece
-}
-
-const SQUARE_MASK: u8 = 0b0011_1111;
-const CAPTURE_FLAG: u8 = 0b0100_0000;
-const EN_PASSANT_FLAG: u8 = 0b1000_0000;
-const PROMOTION_MASK: u8 = 0b1100_0000;
-const PROMOTION_QUEEN: u8 = 0b1100_0000;
-const PROMOTION_ROOK: u8 = 0b1000_0000;
-const PROMOTION_BISHOP: u8 = 0b0100_0000;
-const PROMOTION_KNIGHT: u8 = 0b0000_0000;
-
-impl TTMove {
-    fn is_null(self) -> bool {
-        unsafe { ::std::mem::transmute::<TTMove, u16>(self) == 0 }
-    }
-
-    // Expands this `TTMove` to a `Move`value.
-    pub fn expand(self, pos: &Position) -> Option<Move> {
-        let mut result = Move {
-            from: Square::from(self.from & SQUARE_MASK),
-            to: Square::from(self.to & SQUARE_MASK),
-            piece: pos.find_piece(Square::from(self.from & SQUARE_MASK))?,
-            captured: None,
-            promoted: None,
-            en_passant: self.from & EN_PASSANT_FLAG > 0,
-        };
-
-        if self.from & CAPTURE_FLAG > 0 {
-            if result.en_passant {
-                result.captured = Some(Piece::Pawn);
-            } else {
-                result.captured = pos.find_piece(result.to);
-                result.captured?;
-            }
-        }
-
-        if result.piece == Piece::Pawn && (result.to.rank() == 0 || result.to.rank() == 7) {
-            match self.to & PROMOTION_MASK {
-                PROMOTION_QUEEN => result.promoted = Some(Piece::Queen),
-                PROMOTION_ROOK => result.promoted = Some(Piece::Rook),
-                PROMOTION_BISHOP => result.promoted = Some(Piece::Bishop),
-                PROMOTION_KNIGHT => result.promoted = Some(Piece::Knight),
-                _ => {}
-            }
-        }
-
-        Some(result)
-    }
-}
-
-impl From<Move> for TTMove {
-    fn from(mov: Move) -> TTMove {
-        let from: u8 = mov.from.into();
-        let to: u8 = mov.to.into();
-        let mut result = TTMove {
-            from: from & SQUARE_MASK,
-            to: to & SQUARE_MASK,
-        };
-
-        if mov.captured.is_some() {
-            result.from |= CAPTURE_FLAG;
-        }
-
-        if mov.en_passant {
-            result.from |= EN_PASSANT_FLAG;
-        }
-
-        match mov.promoted {
-            Some(Piece::Queen) => result.to |= PROMOTION_QUEEN,
-            Some(Piece::Rook) => result.to |= PROMOTION_ROOK,
-            Some(Piece::Bishop) => result.to |= PROMOTION_BISHOP,
-            Some(Piece::Knight) => result.to |= PROMOTION_KNIGHT,
-            _ => {}
-        }
-
-        result
-    }
-}
-
-const NUM_CLUSTERS: usize = 4;
 
 impl TT {
     pub fn new(bits: u64) -> Self {
@@ -286,3 +145,144 @@ impl TT {
         None
     }
 }
+
+#[repr(align(64))]
+pub struct Bucket([TTEntry; NUM_CLUSTERS]);
+const NUM_CLUSTERS: usize = 4;
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+pub struct TTEntry {
+    key: Hash,             // 8 byte
+    pub best_move: TTMove, // 2 byte
+    pub depth: Depth,      // 2 byte
+    pub score: TTScore,    // 2 byte
+    pub bound: Bound,      // 1 byte
+    generation: u8,        // 1 byte
+}
+
+impl Default for TTEntry {
+    fn default() -> Self {
+        TTEntry {
+            key: 0,
+            depth: 0,
+            score: TTScore(0),
+            best_move: TTMove { from: 0, to: 0 },
+            bound: 0,
+            generation: 0,
+        }
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+pub struct TTMove {
+    from: u8, // bits 0-5: from positions, bit 6: capture, bit 7: en passant
+    to: u8,   // bits 0-5: to position, bits 6-7 promotion piece
+}
+
+const SQUARE_MASK: u8 = 0b0011_1111;
+const CAPTURE_FLAG: u8 = 0b0100_0000;
+const EN_PASSANT_FLAG: u8 = 0b1000_0000;
+const PROMOTION_MASK: u8 = 0b1100_0000;
+const PROMOTION_QUEEN: u8 = 0b1100_0000;
+const PROMOTION_ROOK: u8 = 0b1000_0000;
+const PROMOTION_BISHOP: u8 = 0b0100_0000;
+const PROMOTION_KNIGHT: u8 = 0b0000_0000;
+
+impl TTMove {
+    fn is_null(self) -> bool {
+        unsafe { ::std::mem::transmute::<TTMove, u16>(self) == 0 }
+    }
+
+    // Expands this `TTMove` to a `Move`value.
+    pub fn expand(self, pos: &Position) -> Option<Move> {
+        let mut result = Move {
+            from: Square::from(self.from & SQUARE_MASK),
+            to: Square::from(self.to & SQUARE_MASK),
+            piece: pos.find_piece(Square::from(self.from & SQUARE_MASK))?,
+            captured: None,
+            promoted: None,
+            en_passant: self.from & EN_PASSANT_FLAG > 0,
+        };
+
+        if self.from & CAPTURE_FLAG > 0 {
+            if result.en_passant {
+                result.captured = Some(Piece::Pawn);
+            } else {
+                result.captured = pos.find_piece(result.to);
+                result.captured?;
+            }
+        }
+
+        if result.piece == Piece::Pawn && (result.to.rank() == 0 || result.to.rank() == 7) {
+            match self.to & PROMOTION_MASK {
+                PROMOTION_QUEEN => result.promoted = Some(Piece::Queen),
+                PROMOTION_ROOK => result.promoted = Some(Piece::Rook),
+                PROMOTION_BISHOP => result.promoted = Some(Piece::Bishop),
+                PROMOTION_KNIGHT => result.promoted = Some(Piece::Knight),
+                _ => {}
+            }
+        }
+
+        Some(result)
+    }
+}
+
+impl From<Move> for TTMove {
+    fn from(mov: Move) -> TTMove {
+        let from: u8 = mov.from.into();
+        let to: u8 = mov.to.into();
+        let mut result = TTMove {
+            from: from & SQUARE_MASK,
+            to: to & SQUARE_MASK,
+        };
+
+        if mov.captured.is_some() {
+            result.from |= CAPTURE_FLAG;
+        }
+
+        if mov.en_passant {
+            result.from |= EN_PASSANT_FLAG;
+        }
+
+        match mov.promoted {
+            Some(Piece::Queen) => result.to |= PROMOTION_QUEEN,
+            Some(Piece::Rook) => result.to |= PROMOTION_ROOK,
+            Some(Piece::Bishop) => result.to |= PROMOTION_BISHOP,
+            Some(Piece::Knight) => result.to |= PROMOTION_KNIGHT,
+            _ => {}
+        }
+
+        result
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct TTScore(Score);
+
+impl TTScore {
+    pub fn to_score(self, ply: Ply) -> Score {
+        if self.0 < -MATE_SCORE + MAX_PLY {
+            self.0 + ply
+        } else if self.0 > MATE_SCORE - MAX_PLY {
+            self.0 - ply
+        } else {
+            self.0
+        }
+    }
+
+    pub fn from_score(score: Score, ply: Ply) -> TTScore {
+        if score < -MATE_SCORE + MAX_PLY {
+            TTScore(score - ply)
+        } else if score > MATE_SCORE - MAX_PLY {
+            TTScore(score + ply)
+        } else {
+            TTScore(score)
+        }
+    }
+}
+
+pub type Bound = u8;
+pub const LOWER_BOUND: Bound = 1;
+pub const UPPER_BOUND: Bound = 2;
+pub const EXACT_BOUND: Bound = LOWER_BOUND | UPPER_BOUND;
+
