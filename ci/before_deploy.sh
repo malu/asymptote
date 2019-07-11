0c7c77c02a12a50391dca21e00ea59ec6cebed09
Maximilian Lupke <maxlupke@gmail.com>
2019-07-11 23:19:37 +0200

    Add builds for broadwell cpus

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,7 +18,7 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --target $TARGET --release -- -C lto -C target-feature=$FEATURES
+    cross rustc --target $TARGET --release -- -C lto -C target-feature=$FEATURES -C target-cpu=$CPU
 
     # TODO Update this to package the right artifacts
     cp target/$TARGET/release/asymptote $stage/
