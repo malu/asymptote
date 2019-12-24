c1d697360e9a19cf2f0589e525541be24aa5d83c
Maximilian Lupke <maximilian@upsellguru.com>
2019-12-24 01:55:15 +0100

    Use PEXT if supported by target architecture
    No functional change

--- src/magic.rs
+++ src/magic.rs
@@ -49,6 +49,14 @@ pub struct Magic {
 }
 
 impl Magic {
+    #[cfg(all(target_arch = "x86_64", target_feature = "bmi2"))]
+    pub fn index(&self, occupied: Bitboard) -> usize {
+        use std::arch::x86_64::_pext_u64;
+
+        self.offset as usize + unsafe { _pext_u64(occupied.0, self.mask.0) as usize }
+    }
+
+    #[cfg(not(all(target_arch = "x86_64", target_feature = "bmi2")))]
     pub fn index(&self, occupied: Bitboard) -> usize {
         let shift = self.magic.wrapping_shr(56) as u32;
         self.offset as usize
